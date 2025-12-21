fbc09ac2daf3fc5cd9892a22fcbd20a8e08cb318
Krishma Kumari <118874025+Krishmakumari@users.noreply.github.com>
2025-12-22 01:31:00 +0530

    Add files via upload

---
+++ Day3Demo/StudnetManagementSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using StudnetManagementSystem;
+
+StudentBL sBLobj=new StudentBL();
+
+sBLobj.AcceptStudentDetails();
+//sBLobj.CalcTotal();
+//sBLobj.CalcAvg();
+
+int t1;//Total
+float p1;//Percentage
+
+sBLobj.CalcResult(out t1,out p1);
+
+System.Console.WriteLine($"Total {t1}");
+System.Console.WriteLine( $"Percentage :{p1}");
