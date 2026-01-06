279f499676205b383dd5baa14887c38f56c084ab
Krishma Kumari <118874025+Krishmakumari@users.noreply.github.com>
2026-01-06 10:34:38 +0530

    Add files via upload

---
+++ LPUMngmentSystem/LPU_BL/StudentBL.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using LPU_Common;
+using System.Collections.Generic;
+using LPU_Entity;
+using LPU_DAL;
+using LPU_Exceptions;
+namespace LPU_BL
+{
+    public class StudentBL : IStudentCRUD
+    {
+        StudentDAO sDAO = null;
+        /// bad practice, as then there will be already one object present without invokinng the constructor
+        /// Student s1 = new Student();
+        public StudentBL()
+        {
+            sDAO = new StudentDAO();
+        }
+        public bool DropStudentDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EnrollStudent(Student sObj)
+        {
+            return sDAO.EnrollStudent(sObj);
+
+        }
+
+        public Student SearchStudentByID(int rollNo)
+        {
+            Student s1 = null;
+            try
+            {
+               s1 = sDAO.SearchStudentByID(rollNo);
+            }
+            catch(LpuException e)
+            {
+                throw e;
+            }
+            
+            return s1;
+        }
+
+        public List<Student> SearchStudentByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateStudentDetails(int id, Student newObj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
