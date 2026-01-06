9c4020ed319e15589ef7ce2c1a5412f2645d9955
Krishma Kumari <118874025+Krishmakumari@users.noreply.github.com>
2026-01-06 10:36:35 +0530

    System with architecture pattern

---
+++ LPUMngmentSystem/LPU_Common/GenericClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPU_Common
+{
+    /// <summary>
+    /// Custom generic class created for Demo purpose
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal class GenericClass<T>
+    {
+        
+        /// <summary>
+        /// Custom Generic Method
+        /// </summary>
+        /// <param name="obj1"></param>
+        /// <param name="obj2"></param>
+        public void SwapMe(ref T obj1,ref T obj2)
+        {
+            T temp;
+            temp=obj1;
+            obj1 = obj2;
+            obj2 = temp;
+        }
+    }
+}
---
+++ LPUMngmentSystem/LPU_Common/IStudentCRUD.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using LPU_Entity;
+using System.Collections.Generic;
+
+namespace LPU_Common
+{
+    public interface IStudentCRUD
+    {
+        Student SearchStudentByID(int rollNo);
+        List<Student> SearchStudentByName(string name);
+        bool EnrollStudent(Student sObj);
+        bool UpdateStudentDetails(int id, Student newObj);
+        bool DropStudentDetails(int id);
+    }
+}
