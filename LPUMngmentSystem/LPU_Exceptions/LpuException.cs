27b00e0d4072d90e12eaf86048e8c12bcf9b98dd
Krishma Kumari <118874025+Krishmakumari@users.noreply.github.com>
2026-01-06 10:40:14 +0530

    System with architecture pattern

---
+++ LPUMngmentSystem/LPU_Exceptions/LpuException.cs
@@ -0,0 +1,19 @@
+﻿namespace LPU_Exceptions
+{
+    /// <summary>
+    /// Custom Exception class created for LPU Project
+    /// by Krishma on Date 29/12/2025 at 11:34 AM
+    /// </summary>
+    public class LpuException : Exception
+    {
+        public LPU_Exception() : base()
+        {
+
+        }
+
+        public LPU_Exception(string errorMsg) :base(errorMsg)
+        {
+
+        }
+    }
+}
