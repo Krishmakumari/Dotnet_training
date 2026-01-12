f80001af16c9fbd1fc3b980454d207cbea2ee2f2
Krishma Kumari <118874025+Krishmakumari@users.noreply.github.com>
2026-01-12 09:55:29 +0530

    File Handling

---
+++ FileHandlingDemo/FileHandlingDemo/DirectoryDemo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandlingDemo
+{
+    public class DirectoryDemo
+    {
+        public void DirectoryDemoFunc(string directoryName)
+        {
+            if (Directory.Exists(directoryName))
+            {
+                Console.WriteLine("Directory ALready exists");
+            }
+            else
+            {
+                Directory.CreateDirectory(directoryName);
+                Console.WriteLine("Directory created");
+            }
+        }
+
+        public void DriveInfoFunc(string driveName)
+        {
+            DriveInfo dInfo = new DriveInfo(driveName);
+            Console.WriteLine($"Drive Name {dInfo.Name}");
+            Console.WriteLine($"Drive FileType {dInfo.DriveType}");
+            Console.WriteLine($"Drive Size {dInfo.TotalSize}");
+        }
+
+        public void PathDemo()
+        {
+            string s = @"D:\Dotnet\FileHandlingDemo";
+            Console.WriteLine(Path.GetFileName(s));
+            Console.WriteLine(Path.GetTempPath);
+        }
+
+
+
+    }
+}
---
+++ FileHandlingDemo/FileHandlingDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using FileHandlingDemo;
+
+DirectoryDemo dObj = new DirectoryDemo();
+//dObj.DirectoryDemoFunc("LPU");
+
+//dObj.DriveInfoFunc("D:\\");
+
+
+//dObj.PathDemo();
+
+FileStreamDemo fsDemoObj = new FileStreamDemo();
+//fsDemoObj.CreateFile(@"D:\Dotnet\FileHandlingDemo");
+
+fsDemoObj.ReadFile(@"C:\Users\Admin\Downloads\front end_Interview_Feedback_Jul 25 2025.txt");
