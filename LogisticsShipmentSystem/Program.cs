5a9d57fa8c77c2d8c96bc4c6b0cc58a404f40d45
Krishma Kumari <118874025+Krishmakumari@users.noreply.github.com>
2026-02-14 12:49:56 +0530

    Logistics Pro Shipment System

---
+++ LogisticsShipmentSystem/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        
+        ShipmentDetails sObj=new ShipmentDetails();
+
+        Console.WriteLine("Enter Shipment code");
+        sObj.shipmentCode=Console.ReadLine();
+
+
+        bool validate=sObj.ValidateShipmentCode();
+
+        if(!validate)
+        {
+            Console.WriteLine("Invalid shipment Code");
+        }
+        else
+        {
+            Console.WriteLine("Enter Transport Mode: ");
+            sObj.transportMode=Console.ReadLine();
+
+            Console.WriteLine("Enter Weight: ");
+            sObj.weight=double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Storage Days: ");
+            sObj.storageDays=Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The total Shipping cost is : {sObj.CalculateTotalCost():F2}");
+
+        }
+
+
+    }
+}
