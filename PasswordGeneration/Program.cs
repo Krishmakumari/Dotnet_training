27ee2a515cbd01a2d24f316019c990a881b5a5d2
Krishma Kumari <118874025+Krishmakumari@users.noreply.github.com>
2026-02-17 12:11:45 +0530

    String Question

---
+++ PasswordGeneration/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+public class Program
+{
+    public static string PassGen(string str)
+    {
+        string r=@"^[A-Z]{4}@(10[0-9]|11[0-5])$";
+        string ans="TECH_";
+        if(!Regex.IsMatch(str,r))
+        {
+            return $"{str} is an invalid username.";
+        }
+        else
+        {
+            int sum=0;
+            for(int i=0;i<4;i++)
+            {
+                char c=char.ToLower(str[i]);
+                sum+=(int) c;
+            }
+            ans+=sum;
+            ans+=str[str.Length-2];
+            ans+=str[str.Length-1];
+        }
+
+        return ans;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Enter the username: ");
+        string str=Console.ReadLine();
+
+        Console.WriteLine($"Password: {Program.PassGen(str)}");
+
+    }
+    
+}
